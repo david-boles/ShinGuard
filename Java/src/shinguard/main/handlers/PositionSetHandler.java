322670d051f1f6f07ad5c0dc42ec27d524fdb9e5
David Boles <david.e.boles22@gmail.com>
2016-08-26 17:43:50 -0700

    update
    update test

--- Java/src/shinguard/main/handlers/PositionSetHandler.java
+++ Java/src/shinguard/main/handlers/PositionSetHandler.java
@@ -36,7 +36,7 @@ public class PositionSetHandler implements HttpHandler {
 			System.out.println("Latitude: " + latiVal);
 			System.out.println("Longitude: " + longVal);
 						
-			HandlerFs.respondHTML(arg0, HandlerFs.CODE_OK, "got it!!");
+			HandlerFs.respondHTML(arg0, HandlerFs.CODE_OK, "got it!");
 			
 			DataManager.registerPosition(idVal, longVal, latiVal);
 		}catch (Exception e) {
