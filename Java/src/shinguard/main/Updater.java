6f7b5d32da1edfc9870c497c5c27bb216a497f11
David Boles <david.e.boles22@gmail.com>
2016-08-26 17:39:57 -0700

    update
    Changed to "git pull" for update

--- Java/src/shinguard/main/Updater.java
+++ Java/src/shinguard/main/Updater.java
@@ -5,7 +5,7 @@ public class Updater {
 	public static void main(String[] args) {
 		Process p;
 		try {
-			p = Runtime.getRuntime().exec("git reset --hard origin/master");
+			p = Runtime.getRuntime().exec("git pull");
 	        p.waitFor();
 	        p = Runtime.getRuntime().exec("java -jar Start.jar");
 		} catch (Exception e) {
